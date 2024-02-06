94232c0c80388cb7eb5e475e7cf9323f7e3ccec6
Rafał Cholewa <148866706+Czasku@users.noreply.github.com>
2024-02-06 11:06:24 +0100

    Add files via upload

---
+++ T2s1.cpp
@@ -0,0 +1,26 @@
+struct Obraz
+{
+	char nazwa[200];
+	double wartosc;
+	char autor[100];
+	bool dostepnosc;
+};
+
+int najwyzszaCena(Obraz dane, char* autor)
+// Funkcja ma wczytać tablicę obrazow i autora
+// a następnie zwrocić nadroższy obraz tego autora.
+{
+	int wartosc;
+	return wartosc;
+}
+
+int main()
+{
+	Obraz bazgroly[] =
+	{
+		{"Ja i kury cioci Jadzi",2.99,"Ja",true},
+		{"Dom na tle wysypiska",4.89,"Pan Zdzisław z Wąchocka",false},
+		{"Plama ropy?",501.29,"P.B. Bezołowiowy",true},
+		// dodaj jeszcze prznajmniej 4 instancje
+	};
+}
